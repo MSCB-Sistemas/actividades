6fb2873d40922ab2bd9089af5369744e8d06ef85
OrellanaGalo <66918990+OrellanaGalo@users.noreply.github.com>
2026-02-09 11:47:33 -0300

    Update actividades.php

--- actividades.php
+++ actividades.php
@@ -15,8 +15,18 @@ $lugar=$_GET["txt_lugar"];
         <option  value="" selected="selected"></option>
         <?php
 			
-		$query_actividades="select id_actividad ,actividad,anio_desde,anio_hasta,categoria,horarios,grupo,periodo from actividades
-						where lugar='$lugar' and activo=1 order by actividad";
+		$query_actividades="SELECT 
+								id_actividad,
+								actividad,
+								anio_desde,
+								anio_hasta,
+								categoria,
+								horarios,
+								grupo,
+								periodo 
+							FROM actividades
+							WHERE lugar='$lugar' AND activo=1 ORDER BY actividad";
+
 		$recordset= mysqli_query($link_mysql,$query_actividades);
 
  		while($actividad=mysqli_fetch_array($recordset)){ 
@@ -24,8 +34,7 @@ $lugar=$_GET["txt_lugar"];
   		?>
         <option value="<?php echo $actividad["id_actividad"]; ?>" ><?php echo $actividad["actividad"]." - "."CATEGORIA: ".$actividad["anio_desde"]." a ".$actividad["anio_hasta"]." ".utf8_encode($actividad["horarios"]." - "."GRUPO: ".$actividad["grupo"]." - "."PERIODO: ".$actividad["periodo"]);?></option>
         <?php
-    
-  			}
+  		}
 		?>
       </select>
 	</div>
